42daf2c8be09072fff60fb8ae303734847f7ec8d
Tran Dinh <trandinh@MacBook-Pro-cua-Tran.local>
2022-09-30 10:35:56 +0700

    done

---
+++ src/main/java/com/codegym/springsecurity_bai18/SpringSecurityBai18Application.java
@@ -0,0 +1,12 @@
+package com.codegym.springsecurity_bai18;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBai18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBai18Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/springsecurity_bai18/SpringSecurityBai18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.springsecurity_bai18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBai18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
